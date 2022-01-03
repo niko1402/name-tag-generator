7be8091ec5d767c48ad376319b2e0a3e945a7eaf
nik_e_p <nikpayton@gmail.com>
2022-01-03 10:59:22 -0300

    Added life-cycle methods to App file

--- src/App.js
+++ src/App.js
@@ -4,27 +4,37 @@ import UserInput from "./UserInput";
 
 class App extends Component {
   state = {
-    names: ["Erin", "Ann", "Nichole", "Sharon", "Maryn"]
+    names: []
   };
   removeName = (clickedIndex) => {
     // to learn how the .filter method works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-    // JS convention: Use _ to indicate to JS that you aren't using another parameter here, but thus keeping the correct order of params 
+    // JS convention: Use _ to indicate to JS that you aren't using another parameter here, but thus keeping the correct order of params
     const filterCallback = (_, index) => index !== clickedIndex;
     const newNames = this.state.names.filter(filterCallback);
     this.setState({ names: newNames });
   };
-    // Tell it accept a name as the prop. Add the new name to the aray of name, and ALSO
-    // the spread of names in the array so far
+  // Tell it accept a name as the prop. Add the new name to the aray of name, and ALSO
+  // the spread of names in the array so far
   addName = (name) => {
-    const addNewName = [name, ...this.state.names]
+    const addNewName = [name, ...this.state.names];
     this.setState({ names: addNewName });
   };
-
+  componentDidUpdate() {
+    const savedNamesString = JSON.stringify(this.state.names);
+    localStorage.setItem("savedNames", savedNamesString);
+  }
+  componentDidMount() {
+    const savedNamesString = localStorage.getItem("savedNames");
+    if (savedNamesString) {
+      const savedNames = JSON.parse(savedNamesString);
+      this.setState({ names: savedNames });
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>Name Tag Generator</h1>
-        <UserInput addName={ this.addName }/>
+        <UserInput addName={this.addName} />
         <NameTagList names={this.state.names} removeName={this.removeName} />
       </div>
     );
